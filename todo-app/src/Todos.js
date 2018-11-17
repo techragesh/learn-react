039f58a91c6b8d735357308b932c68be0a173326
ragesh <ragesh@Users-iMac.local>
2018-11-17 23:19:41 +0100

    todoapp project initial version

---
+++ todo-app/src/Todos.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+const Todos = ({ todos, deleteTodo }) => {
+  const todolist = todos.length ? (
+    todos.map(todo => {
+      return (
+        <div className="collection-item" key={todo.id}>
+          <span onClick={() => deleteTodo(todo.id)}>{todo.content}</span>
+        </div>
+      );
+    })
+  ) : (
+    <p className="center">No Todos Found</p>
+  );
+  return <div className="todos collection">{todolist}</div>;
+};
+
+export default Todos;
